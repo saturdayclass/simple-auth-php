25c35fa83cda5c6a7ea5949d4ce96200d6bdddcc
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2021-01-02 14:21:20 +0700

    update: added register form

---
+++ register.php
@@ -0,0 +1,66 @@
+<?php
+  include "koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Halaman Register</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+</head>
+<body>
+
+  <section class="row">
+    <div class="col-md-6 offset-md-3 align-self-center"> 
+      <h1 class="text-center mt-4">Form Register</h1>
+      <form method="POST">
+        <div class="mb-3">
+          <label for="inputNama" class="form-label">Nama</label>
+          <input type="text" class="form-control" id="inputNama" name="nama" placeholder="Masukan Nama">
+        </div>
+        <div class="mb-3">
+          <label for="inputUsername" class="form-label">Username</label>
+          <input type="text" class="form-control" id="inputUsername" name="username" placeholder="Masukan Username">
+        </div>
+        <div class="mb-3">
+          <label for="inputPassword" class="form-label">Password</label>
+          <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Masukan Password">
+        </div>
+        <input name="daftar" type="submit" class="btn btn-primary" value="Daftar">
+        <a href="index.php" type="button" class="btn btn-info text-white">Kembali</a>
+      </form>
+    </div>
+  </section>
+
+  <?php
+    
+    // Buat kondisi jika tombol data di klik
+    if(isset($_POST['daftar'])){
+      // Membuat variable setiap field inputan agar kodingan lebih rapi.
+      $nama = $_POST['nama'];
+      $username = $_POST['username'];
+      $password = md5($_POST['password']);
+
+      // Membuat Query
+      $query = "INSERT INTO tb_user (nama, username, password) VALUES('".$nama."', '".$username."', '".$password."')";
+
+      $result = mysqli_query($koneksi, $query);
+
+      if($result){
+        header("location: index.php");
+      } else {
+        echo "<script>alert('Data Gagal di tambahkan!')</script>";
+      }
+
+    }    
+
+  ?>
+
+</body>
+</html>
